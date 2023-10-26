4c1f9b3f94d4027a4763e901e4bf628685d2857e
HiBorn4 <hiborn4@gmail.com>
2023-10-26 21:08:44 +0530

    responsive design without MediaQuery

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
-# my
+Your task is to recreate a responsive design in Flutter where the component size adjusts based on the screen size.
 
-A new Flutter project.
+Instructions:
 
-## Getting Started
+Recreate a Responsive Design: Your objective is to recreate a design that demonstrates responsiveness in Flutter. The design should consist of components whose sizes adapt to different screen sizes. You can use a design from Figma, a whiteboard sketch, or any other source of your choice.
 
-This project is a starting point for a Flutter application.
+Responsive Components: Focus on creating components that adjust their sizes dynamically according to the screen dimensions. This will highlight your understanding of responsive UI development in Flutter.
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Task Description: Create cards that follow the responsive-design pattern. Sepcifically, the task require
+you to create 2 differen cards on the same screen:
+Card 1 (The card at the top):
+As the screen size grows or shrinks, the width of the card changes to occupy the same porpotion of
+the screen. Furthermore, the green-area inside it also grows to take the same proportion of the screen
+as before.
+Card 2 (The card at the bottom):
+Ad the screen size grows or shrinks, the width of the card changes to occupy the same poportion of
+the screen. Furthermore, the green-area inside it also changes. Also note that, even though the size of
+the card is changing, the grey-area still maintains its size and remains at the top-center of the card.
+Note:
+* The while area represent a screen (mobile screen, web-browser screen, ipad screen, etc).
+• The red-ish area is being refered to as card.
+* Extra points if you can implement this without using "MediaQuery".
--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,179 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double screenWidth = constraints.maxWidth;
+        double greyCardWidth = screenWidth * 0.25; // 20% of screen width
+
+        return MaterialApp(
+          home: Scaffold(
+            body: Center(
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.white, // Representing the screen background
+                child: Column(
+                  children: [
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Container(
+                              height: 60,
+                            ),
+                            CardOne(),
+                            Container(
+                              height: 150,
+                            ),
+                            CardTwo(),
+                          ],
+                        ),
+                        Positioned(
+                          top:
+                              328, // Half above CardTwo and half above the red card
+                          child: Container(
+                            color: Colors.grey[
+                                400], // Representing the overlaying grey card
+                            width: greyCardWidth,
+                            height: 65, // Adjust the height as needed
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class CardOne extends StatelessWidget {
+  // Define the desired ratio for the red card width
+  final double redCardWidthRatio = 0.93; // Adjust as needed
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      height: 150,
+      decoration: BoxDecoration(
+        color: Colors.red[300], // Representing the card background
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey, // Shadow color
+            blurRadius: 10, // Spread of the shadow
+            offset: Offset(0, 5), // Offset of the shadow
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final cardWidth = constraints.maxWidth *
+              redCardWidthRatio; // Calculate red card width based on the ratio
+          final greenAreaWidth = cardWidth * 0.93;
+          final greyAreaWidth = cardWidth * 0.28;
+
+          return Container(
+            width: cardWidth, // Set the red card width
+            padding: EdgeInsets.all(16.0), // Adding padding to the entire card
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment:
+                  CrossAxisAlignment.start, // Align children to the left
+              children: [
+                Container(
+                  color: Colors.grey[400], // Representing the grey card
+                  width: greyAreaWidth,
+                  height: 40, // Adjust the height as needed
+                  alignment: Alignment.center,
+                ),
+                Container(
+                  color: Colors.green[200], // Representing the green area
+                  width: greenAreaWidth,
+                  height: 43, // Adjust the height as needed
+                  alignment: Alignment.center,
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class CardTwo extends StatelessWidget {
+  // Define the desired ratio for the red card width
+  final double redCardWidthRatio = 0.93; // Adjust as needed
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      decoration: BoxDecoration(
+        color: Colors.red[300], // Representing the card background
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey, // Shadow color
+            blurRadius: 10, // Spread of the shadow
+            offset: Offset(0, 5), // Offset of the shadow
+          ),
+        ],
+      ),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final cardWidth = constraints.maxWidth *
+              redCardWidthRatio; // Calculate red card width based on the ratio
+          final greenAreaWidth = cardWidth * 0.98;
+          final greyAreaWidth = cardWidth * 0.28;
+
+          return Container(
+            width: cardWidth, // Set the red card width
+            padding: EdgeInsets.all(16.0), // Adding padding to the entire card
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment:
+                  CrossAxisAlignment.start, // Align children to the left
+              children: [
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      color: Colors
+                          .red[300], // Cover the portion inside the red card
+                      width:
+                          greyAreaWidth / 2, // Half the width of the grey card
+                      height: 38, // Adjust the height as needed
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 5,
+                ),
+                Container(
+                  color: Colors.green[200], // Representing the green area
+                  width: greenAreaWidth,
+                  height: 40, // Adjust the height as needed
+                  alignment: Alignment.center,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
